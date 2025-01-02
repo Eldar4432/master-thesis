2bbfa0fff4daf79749b86f27b989d2b2efa77dbb
Eldar4432 <aibekov4432@gmail.com>
2025-01-02 18:23:41 +0100

    Update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import style from "./Header.module.css";
+// import UnicamLogo from "../../shared/unicamLogo/UnicamLogo";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div>Header</div>
-    </div>
+    <header className={style.header}>
+      <div className={style.logo}>{/* <UnicamLogo /> */}</div>
+      <nav className={style.nav}>
+        <a className={style.nav_links} onClick={() => navigate("/")}>
+          Главная страница
+        </a>
+
+        <a className={style.nav_links} href="#services">
+          Services
+        </a>
+        <a className={style.nav_links} onClick={() => navigate("/profile")}>
+          Personal Page
+        </a>
+      </nav>
+      <button onClick={() => navigate("/login")} className={style.btn}>
+        Sign Up
+      </button>
+    </header>
   );
 };
 
