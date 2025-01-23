6175bcbc8c527421c2a0d5ac670b23d89eb212f9
Eldar4432 <aibekov4432@gmail.com>
2025-01-23 18:39:33 +0100

    created component Jobs

---
+++ src/components/Jobs/Jobs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Jobs = () => {
+  return (
+    <div>
+      <div>Jobs</div>
+      <div>component</div>
+    </div>
+  );
+};
+
+export default Jobs;
--- src/pages/JobsPage/JobsPage.jsx
+++ src/pages/JobsPage/JobsPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Jobs from "../../components/Jobs/Jobs";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const JobsPage = () => {
-  return <div>JobsPage</div>;
+  return (
+    <div>
+      <Header />
+      <Jobs />
+      <Footer />
+    </div>
+  );
 };
 
 export default JobsPage;
